081643b18c19d81f58e46072cf9dcc7a036a769b
yogeshpatel23 <yogesh.d.p.patel@gmail.com>
2021-08-19 17:50:11 +0530

    Scroll complete

--- src/App.js
+++ src/App.js
@@ -1,51 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { useState, useRef, useCallback } from 'react'
 import './App.css';
-import PokemonList from './PokemonList';
-import Pagination from './Pagination';
+import useBookSearch from './useBookSearch';
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
-  const [currentPageUrl, setcurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
-  const [nextPageUrl, setnextPageUrl] = useState()
-  const [prePageUrl, setprePageUrl] = useState()
-  const [loading, setLoading] = useState(true)
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
 
-  useEffect(() => {
-    setLoading(true)
-    let cancel
-    axios.get(currentPageUrl, {
-      cancelToken: new axios.CancelToken(c => cancel = c)
+  const { loading, books, error, hasMore } = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
     })
-      .then(res => {
-        setPokemon(res.data.results.map(p => p.name))
-        setnextPageUrl(res.data.next)
-        setprePageUrl(res.data.previous)
-        setLoading(false)
-      })
 
-    return () => cancel()
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
 
-  }, [currentPageUrl])
 
-  function gotoNextPage() {
-    setcurrentPageUrl(nextPageUrl)
+  function handleInput(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
   }
-
-  function gotoPrePage() {
-    setcurrentPageUrl(prePageUrl)
-  }
-
-  if (loading) return 'Loading...'
 
   return (
     <div className="app">
-      <PokemonList pokemon={pokemon} />
-
-      <Pagination
-        gotoNextPage={nextPageUrl ? gotoNextPage : null}
-        gotoPrePage={prePageUrl ? gotoPrePage : null}
-      />
+      <input type="text" value={query} onChange={handleInput} />
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div className="book" ref={lastBookElementRef} key={book} >{book}</div>
+        } else {
+          return <div className="book" key={book} >{book}</div>
+        }
+      })}
+      <div>{loading && <div className="loading">loading...</div>}</div>
+      <div>{error && 'Error..'}</div>
     </div>
   );
 }
